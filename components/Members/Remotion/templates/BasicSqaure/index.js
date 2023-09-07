86969036008fdb1ecc7e086e83b32e91201f93ec
trentnixon <trentnixon@gmail.com>
2023-09-07 16:41:07 +1000

    remove theem provider

--- components/Members/Remotion/templates/BasicSqaure/index.js
+++ components/Members/Remotion/templates/BasicSqaure/index.js
@@ -89,7 +89,7 @@ const Template_Basic_Sqaure = (props) => {
 
   //console.log("TIMINGS", TIMINGS, TIMINGS.FPS_INTRO);
    return (
-    <ThemeProvider theme={THEME}>
+   
       <AbsoluteFill style={{ backgroundColor: THEME.primary }}>
         <Series>
           <Series.Sequence durationInFrames={TIMINGS.FPS_INTRO}>
@@ -125,7 +125,7 @@ const Template_Basic_Sqaure = (props) => {
           src={`${DATA.VIDEOMETA.Video.audio_option}`}
         />
       </AbsoluteFill>
-    </ThemeProvider>
+   
   );
 
 
