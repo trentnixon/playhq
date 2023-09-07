79058a64d7752162a49a70fc6b3e55fb0ecf64f0
trentnixon <trentnixon@gmail.com>
2023-09-07 13:31:20 +1000

    move to obj

--- components/Members/Remotion/ThemePreviewer.js
+++ components/Members/Remotion/ThemePreviewer.js
@@ -14,11 +14,60 @@ const RemotionPreview = ({ setIsPlaying, DATA }) => {
     "Basic Sqaure": Template_Basic_Sqaure,
     "Basic Rounded": Template_Basic_Rounded,
   };
+  const ASSETDATA = DATA.DATA;
 
-  //console.log("RemotionPreview DATA CHECK", DATA.DATA.VIDEOMETA);
-  /* const playerRef = useRef(null); */
+  if (typeof OBJ[ASSETDATA.VIDEOMETA.Video.Template] === "undefined") {
+    console.error("Template component is undefined");
+  }
+  const HasSponsors = () => {
+    ASSETDATA.VIDEOMETA.Video.includeSponsors;
+    if (ASSETDATA.VIDEOMETA.Club.Sponsors.length === 0) return 0;
+    return ASSETDATA.VIDEOMETA.Video.includeSponsors
+      ? ASSETDATA.VIDEOMETA.Video.TIMINGS.FPS_OUTRO
+      : 0;
+  };
 
-  /* useEffect(() => {
+  const Create = {
+    ratio: { width: 1440, height: 1920 },
+    fps: 30,
+    CompositionID: ASSETDATA.VIDEOMETA.Video.CompositionID,
+    Template: OBJ[ASSETDATA.VIDEOMETA.Video.Template],
+    durationInFrames: [
+      ASSETDATA.VIDEOMETA.Video.TIMINGS.FPS_INTRO,
+      HasSponsors(),
+      ASSETDATA.VIDEOMETA.Video.TIMINGS.FPS_MAIN,
+    ].reduce((a, b) => a + b, 0),
+  };
+
+  console.log("ASSET Create", Create);
+
+  return (
+    <Center>
+      <Player
+        id={Create.CompositionID}
+        component={Create.Template}
+        durationInFrames={Create.durationInFrames}
+        compositionWidth={Create.ratio.width}
+        compositionHeight={Create.ratio.height}
+        fps={Create.fps}
+        numberOfSharedAudioTags={0}
+        inputProps={DATA}
+        controls
+        style={{
+          width: parseInt(Create.ratio.width) * 0.25,
+          height: parseInt(Create.ratio.height) * 0.25,
+        }}
+      />
+    </Center>
+  );
+};
+
+export default RemotionPreview;
+
+//console.log("RemotionPreview DATA CHECK", ASSETDATA.VIDEOMETA);
+/* const playerRef = useRef(null); */
+
+/* useEffect(() => {
     const { current } = playerRef;
     if (!current) {
       return;
@@ -42,12 +91,12 @@ const RemotionPreview = ({ setIsPlaying, DATA }) => {
     };
   }, []); */
 
-  /*   const PlayerOnly = ({ playerRef }) => {
+/*   const PlayerOnly = ({ playerRef }) => {
     return (
       <Player
         ref={playerRef}
-        id={DATA.DATA.VIDEOMETA.Video.CompositionID}
-        component={OBJ[DATA.DATA.VIDEOMETA.Video.Template]}
+        id={ASSETDATA.VIDEOMETA.Video.CompositionID}
+        component={OBJ[ASSETDATA.VIDEOMETA.Video.Template]}
         durationInFrames={550}
         compositionWidth={1440}
         compositionHeight={1920}
@@ -62,47 +111,3 @@ const RemotionPreview = ({ setIsPlaying, DATA }) => {
       />
     );
   }; */
-  console.log("PLAYER DATA", DATA.DATA.VIDEOMETA.Video.TIMINGS);
-  const HasSponsors = () => {
-    DATA.DATA.VIDEOMETA.Video.includeSponsors;
-    if (DATA.DATA.VIDEOMETA.Club.Sponsors.length === 0) return 0;
-    return DATA.DATA.VIDEOMETA.Video.includeSponsors
-      ? DATA.DATA.VIDEOMETA.Video.TIMINGS.FPS_OUTRO
-      : 0;
-  };
-
-  console.log(
-    [
-      DATA.DATA.VIDEOMETA.Video.TIMINGS.FPS_INTRO,
-      HasSponsors(),
-      DATA.DATA.VIDEOMETA.Video.TIMINGS.FPS_MAIN,
-    ].reduce((a, b) => a + b, 0)
-  );
-
-  return (
-    <Center>
-      <Player
-        id={DATA.DATA.VIDEOMETA.Video.CompositionID}
-        component={OBJ[DATA.DATA.VIDEOMETA.Video.Template]}
-    
-        durationInFrames={[
-          DATA.DATA.VIDEOMETA.Video.TIMINGS.FPS_INTRO,
-          HasSponsors(),
-          DATA.DATA.VIDEOMETA.Video.TIMINGS.FPS_MAIN,
-        ].reduce((a, b) => a + b, 0)}
-        compositionWidth={1440}
-        compositionHeight={1920}
-        fps={30}
-        numberOfSharedAudioTags={0}
-        inputProps={DATA}
-        controls
-        style={{
-          width: parseInt(1440) * 0.25,
-          height: parseInt(1920) * 0.25,
-        }}
-      />
-    </Center>
-  );
-};
-
-export default RemotionPreview;
