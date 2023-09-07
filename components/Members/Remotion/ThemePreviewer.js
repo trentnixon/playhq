f4730f6fa04fb327030949fcd4dc4165d747d6f8
trentnixon <trentnixon@gmail.com>
2023-09-07 18:23:15 +1000

    600

--- components/Members/Remotion/ThemePreviewer.js
+++ components/Members/Remotion/ThemePreviewer.js
@@ -50,7 +50,7 @@ const RemotionPreview = ({ setIsPlaying, DATA }) => {
         component={Create.Template}
         compositionWidth={Create.ratio.width}
         compositionHeight={Create.ratio.height}
-        frameToDisplay={500}
+        frameToDisplay={600}
         durationInFrames={Create.durationInFrames}
         fps={30}
         inputProps={DATA}
