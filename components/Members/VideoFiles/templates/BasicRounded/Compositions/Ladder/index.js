328a3f7d6b3333fcad0cf274aea933dd9d3316ad
trentnixon <trentnixon@gmail.com>
2023-09-13 19:00:12 +1000

    Sequence

--- components/Members/VideoFiles/templates/BasicRounded/Compositions/Ladder/index.js
+++ components/Members/VideoFiles/templates/BasicRounded/Compositions/Ladder/index.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import {Series} from 'remotion';
+import React from "react";
+import { Sequence } from "remotion";
 // Components
-import {LadderMain} from './LadderMain';
-import {LogoClubTitleHeader} from '../../Components/Header/LogoClubTitleHeader';
-export const Ladder = ({DATA, theme, fontFamily, FPS_MAIN, FPS_LADDER}) => {
-	return (
-		<Series>
-			<Series.Sequence durationInFrames={FPS_MAIN} layout="none">
-				<LogoClubTitleHeader
-					THEME={theme}
-					fontFamily={fontFamily}
-					DATA={DATA.VIDEOMETA}
-					FPS_MAIN={FPS_MAIN}
-				/>
-				<LadderMain
-					THEME={theme}
-					DATA={DATA.DATA}
-					fontFamily={fontFamily}
-					FPS_MAIN={FPS_MAIN}
-					FPS_LADDER={FPS_LADDER}  
-				/>
-			</Series.Sequence>
-		</Series>
-	);
+import { LadderMain } from "./LadderMain";
+import { LogoClubTitleHeader } from "../../Components/Header/LogoClubTitleHeader";
+export const Ladder = ({ DATA, theme, fontFamily, FPS_MAIN, FPS_LADDER }) => {
+  return (
+    <Sequence durationInFrames={FPS_MAIN} layout="none">
+      <LogoClubTitleHeader
+        THEME={theme}
+        fontFamily={fontFamily}
+        DATA={DATA.VIDEOMETA}
+        FPS_MAIN={FPS_MAIN}
+      />
+      <LadderMain
+        THEME={theme}
+        DATA={DATA.DATA}
+        fontFamily={fontFamily}
+        FPS_MAIN={FPS_MAIN}
+        FPS_LADDER={FPS_LADDER}
+      />
+    </Sequence>
+  );
 };
- 
--- components/Members/VideoFiles/templates/BasicRounded/Compositions/WeekendSingleGameResult/index.js
+++ components/Members/VideoFiles/templates/BasicRounded/Compositions/WeekendSingleGameResult/index.js
@@ -1,33 +1,31 @@
-import React from 'react';
-import {Series} from 'remotion';
+import React from "react";
+import { Sequence, Series } from "remotion";
 
 // Components
-import { LogoClubTitleHeaderLimited} from '../../Components/Header/LogoClubTitleHeader';
-import {Results} from './Results';
+import { LogoClubTitleHeaderLimited } from "../../Components/Header/LogoClubTitleHeader";
+import { Results } from "./Results";
 
 export const WeekendSingleGameResult = ({
-	DATA,
-	theme,
-	fontFamily,
-	FPS_MAIN,
-	FPS_SCORECARD,
+  DATA,
+  theme,
+  fontFamily,
+  FPS_MAIN,
+  FPS_SCORECARD,
 }) => {
-	return (
-		<Series>
-			<Series.Sequence durationInFrames={FPS_MAIN} layout="none">
-				<LogoClubTitleHeaderLimited 
-					THEME={theme}
-					DATA={DATA.VIDEOMETA}
-					FPS_MAIN={FPS_MAIN}
-				/>
-				<Results
-					THEME={theme}
-					DATA={DATA}
-					fontFamily={fontFamily}
-					FPS_MAIN={FPS_MAIN}
-					FPS_SCORECARD={FPS_SCORECARD}
-				/>
-			</Series.Sequence>
-		</Series>
-	);
+  return (
+    <Sequence durationInFrames={FPS_MAIN} layout="none">
+      <LogoClubTitleHeaderLimited
+        THEME={theme}
+        DATA={DATA.VIDEOMETA}
+        FPS_MAIN={FPS_MAIN}
+      />
+      <Results
+        THEME={theme}
+        DATA={DATA}
+        fontFamily={fontFamily}
+        FPS_MAIN={FPS_MAIN}
+        FPS_SCORECARD={FPS_SCORECARD}
+      />
+    </Sequence>
+  );
 };
