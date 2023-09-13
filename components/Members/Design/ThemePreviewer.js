09680abbd78f6181be34f284bb13730fed6e135d
trentnixon <trentnixon@gmail.com>
2023-09-13 18:41:32 +1000

    280

--- components/Members/Design/ThemePreviewer.js
+++ components/Members/Design/ThemePreviewer.js
@@ -81,7 +81,7 @@ const generateJsonForThumbnail = (userAccount, assetType, metadata) => {
         break;
       case "WeekendSingleGameResult":
         jsonData.DATA = DATA_WeekendSingleGameResult;
-        jsonData.VIDEOMETA.Video.frameToDisplay=360;
+        jsonData.VIDEOMETA.Video.frameToDisplay=280;
         break;
       default:
         console.warn(`Unknown asset type: ${assetType}`);
