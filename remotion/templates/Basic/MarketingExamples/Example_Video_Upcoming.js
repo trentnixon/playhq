619b096d6e0f16a10cfc1a000deb026ca0f029e5
trentnixon <trentnixon@gmail.com>
2023-11-18 22:27:36 +1100

    hard code timings

--- remotion/templates/Basic/MarketingExamples/Example_Video_Upcoming.js
+++ remotion/templates/Basic/MarketingExamples/Example_Video_Upcoming.js
@@ -1,81 +1,77 @@
 /* eslint-disable camelcase */
-import {ThemeProvider} from 'styled-components';
-import {Series, AbsoluteFill, Audio, interpolate} from 'remotion';
-import {loadFont} from '@remotion/google-fonts/Heebo';
-import {BGImageAnimation} from '../Components/Common/BGImageAnimation';
-import {TitleSequenceFrame} from '../Components/Intro';
-import {OutroSequenceFrame} from '../Components/Outro';
-import {Fixtures} from '../Compositions/UpcomingFixtures';
-import {CompositionLength} from '../../../utils/helpers';
+import { ThemeProvider } from "styled-components";
+import { Series, AbsoluteFill, Audio, interpolate } from "remotion";
+import { loadFont } from "@remotion/google-fonts/Heebo";
+import { BGImageAnimation } from "../Components/Common/BGImageAnimation";
+import { TitleSequenceFrame } from "../Components/Intro";
+import { OutroSequenceFrame } from "../Components/Outro";
+import { Fixtures } from "../Compositions/UpcomingFixtures";
+import { CompositionLength } from "../../../utils/helpers";
 
 export const Example_Video_Upcoming = (props) => {
-	const {DATA} = props;
-	const {fontFamily} = loadFont();
-	const {TIMINGS} = DATA;
+  const { DATA } = props;
+  const { fontFamily } = loadFont();
+  const { TIMINGS } = DATA;
 
-	const THEME = DATA.VIDEOMETA.Video.Theme;
+  const THEME = DATA.VIDEOMETA.Video.Theme;
 
-	const commonProps = {
-		DATA: DATA.DATA,
-		VIDEOMETA: DATA.VIDEOMETA,
-		TIMINGS: DATA.TIMINGS,
-		THEME: THEME,
-		fontFamily,
-		FPS_MAIN: TIMINGS.FPS_MAIN,
-		FPS_SCORECARD: TIMINGS.FPS_SCORECARD,
-		FPS_LADDER: TIMINGS.FPS_LADDER,
-		TemplateVariation: DATA.VIDEOMETA.Video.TemplateVariation,
-	};
- 
-	console.log(commonProps)
-	return (
-		<ThemeProvider theme={THEME}>
-			<AbsoluteFill>
-				<BGImageAnimation
-					HeroImage={DATA.VIDEOMETA.Video.HeroImage}
-					TIMINGS={TIMINGS.FPS_MAIN + 210}
-					THEME={THEME}
-				/>
-				Outside AbsoluteFill
-				<AbsoluteFill style={{zIndex: 100}}>
-					inside AbsoluteFill
-					<Series>
+  const commonProps = {
+    DATA: DATA.DATA,
+    VIDEOMETA: DATA.VIDEOMETA,
+    TIMINGS: DATA.TIMINGS,
+    THEME: THEME,
+    fontFamily,
+    FPS_MAIN: TIMINGS.FPS_MAIN,
+    FPS_SCORECARD: TIMINGS.FPS_SCORECARD,
+    FPS_LADDER: TIMINGS.FPS_LADDER,
+    TemplateVariation: DATA.VIDEOMETA.Video.TemplateVariation,
+  };
 
-						<Series.Sequence durationInFrames={TIMINGS.FPS_INTRO}>
-						inside
-							<TitleSequenceFrame
-								THEME={THEME}
-								fontFamily={fontFamily}
-								FPS_INTRO={TIMINGS.FPS_INTRO}
-								VIDEOMETA={DATA.VIDEOMETA}
-							/>
-						</Series.Sequence>
-						<Series.Sequence durationInFrames={TIMINGS.FPS_MAIN}>
-							HERE
-							<Fixtures {...commonProps} />
-						</Series.Sequence>
-						<Series.Sequence durationInFrames={TIMINGS.FPS_OUTRO}>
-							<OutroSequenceFrame
-								theme={THEME}
-								fontFamily={fontFamily}
-								FPS={TIMINGS.FPS_OUTRO}
-								DATA={DATA}
-							/>
-						</Series.Sequence>
-					</Series>
-				</AbsoluteFill>
-				<Audio
-					volume={(f) =>
-						interpolate(
-							f,
-							[CompositionLength(DATA) - 30, CompositionLength(DATA)],
-							[0.7, 0],
-							{extrapolateLeft: 'clamp'}
-						)
-					}
-					src={`${DATA.VIDEOMETA.Video.audio_option}`}
-				/>
-			</AbsoluteFill>
-		</ThemeProvider>
-	);
+  console.log(commonProps);
+  return (
+    <ThemeProvider theme={THEME}>
+      <AbsoluteFill>
+        <BGImageAnimation
+          HeroImage={DATA.VIDEOMETA.Video.HeroImage}
+          TIMINGS={TIMINGS.FPS_MAIN + 210}
+          THEME={THEME}
+        />
+
+        <AbsoluteFill style={{ zIndex: 100 }}>
+          <Series>
+            <Series.Sequence durationInFrames={90}>
+              <TitleSequenceFrame
+                THEME={THEME}
+                fontFamily={fontFamily}
+                FPS_INTRO={90}
+                VIDEOMETA={DATA.VIDEOMETA}
+              />
+            </Series.Sequence>
+            <Series.Sequence durationInFrames={540}>
+              <Fixtures {...commonProps} />
+            </Series.Sequence>
+            <Series.Sequence durationInFrames={180}>
+              <OutroSequenceFrame
+                theme={THEME}
+                fontFamily={fontFamily}
+                FPS={TIMINGS.FPS_OUTRO}
+                DATA={DATA}
+              />
+            </Series.Sequence>
+          </Series>
+        </AbsoluteFill>
+        <Audio
+          volume={(f) =>
+            interpolate(
+              f,
+              [CompositionLength(DATA) - 30, CompositionLength(DATA)],
+              [0.7, 0],
+              { extrapolateLeft: "clamp" }
+            )
+          }
+          src={`${DATA.VIDEOMETA.Video.audio_option}`}
+        />
+      </AbsoluteFill>
+    </ThemeProvider>
+  );
 };
