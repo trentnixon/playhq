24e250adf690327b909859f5ff3f0a7842e00e94
trentnixon <trentnixon@gmail.com>
2024-05-08 22:31:21 +1000

    fix sidebar logic for account status

--- components/Layouts/members/Members_Account_Details_Card/components/useUserStatus.js
+++ components/Layouts/members/Members_Account_Details_Card/components/useUserStatus.js
@@ -59,9 +59,58 @@ function calculateDaysLeft(endOrderAt) {
   return Math.ceil((endDate - today) / (1000 * 60 * 60 * 24));
 }
 
+
 export function useUserStatus(user) {
   const orders = user.attributes.orders?.data ?? [];
 
+  if (orders.length === 0) {
+    return {
+      statusMessage: "No Orders Found",
+      statusColor: "gray.4",
+      StatusIcon: IconX, // Assuming IconX is an appropriate icon for "no orders"
+      daysLeft: null,
+    };
+  }
+
+ // Find the most recent order by the highest ID value
+ const mostRecentOrder = orders.reduce((latest, order) => {
+  return parseInt(order.id) > parseInt(latest.id) ? order : latest;
+}, orders[0]);
+
+console.log("mostRecentOrder ", mostRecentOrder);
+  try {
+    const { createdAt, endOrderAt, startOrderAt, subscription_tier } = mostRecentOrder.attributes;
+    const { statusMessage, statusColor, StatusIcon } = getStatus(mostRecentOrder.attributes);
+    const { Title, DaysInPass, includeSponsors } = subscription_tier?.data?.attributes || {};
+    const daysLeft = calculateDaysLeft(endOrderAt);
+
+    return {
+      createdAt: new Date(createdAt),
+      statusMessage,
+      statusColor,
+      StatusIcon,
+      Title,
+      DaysInPass,
+      includeSponsors,
+      daysLeft,
+      endOrderAt,
+      startOrderAt,
+    };
+  } catch (error) {
+    console.error("Failed to process the most recent order:", error);
+    return {
+      statusMessage: "Error processing the order",
+      statusColor: "error",
+      StatusIcon: IconError, // Assume IconError is defined elsewhere
+      daysLeft: null,
+    };
+  }
+}
+
+/* export function useUserStatus(user) {
+  const orders = user.attributes.orders?.data ?? [];
+ 
+  console.log("orders ", orders)
   const orderStatuses = orders.map((order) => {
     const { createdAt, endOrderAt, startOrderAt, subscription_tier } =
       order.attributes;
@@ -100,7 +149,7 @@ export function useUserStatus(user) {
         StatusIcon: IconX,
         daysLeft: null,
       };
-}
+} */
 
 /*
 {
